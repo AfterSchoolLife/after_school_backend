f93dd0130e0b36729fb0f723caa8e70a9d0c8cef
Syed Jawad Bukhari <syedjawadbukhari5@gmail.com>
2024-12-07 01:13:35 +0500

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,9 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # origin of the production site
     # origins 'https://after-school-frontend.vercel.app'
-    origins 'http://localhost:3000'
+    origins 'https://after-school-frontend-tio8.vercel.app'
+
+    # origins 'http://localhost:3000'
     resource(
       '*',
       headers: :any,
