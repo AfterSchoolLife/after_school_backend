d8de86da0024c5f193a407018f3e2cd1a75321c2
Syed Jawad Bukhari <syedjawadbukhari5@gmail.com>
2024-12-08 11:27:46 +0500

    display images on cloud

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,6 +88,9 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
   # config.assets.compile = true
+
+  config.active_storage.default_url_options = { host: 'https://after-school-frontend-tio8.vercel.app' }
+
   config.serve_static_assets = true
 
   config.action_mailer.delivery_method = :smtp
